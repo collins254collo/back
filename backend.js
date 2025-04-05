5a3ee0b3ad0429866e87f1b2eb9d0951908a078d
Collins Njogu <njogucollins10397@gmail.com>
2025-04-05 12:55:09 +0300

    backend doing wonders

---
+++ backend.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+
+const express = require("express");
+const nodemon = require("nodemon");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com", // Correct hostname
+  port: 587, // Port for TLS
+  secure: false, // Use TLS (not SSL)
+
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+app.post("/send_email", async (req, res) => {
+  const { name, email, message } = req.body;
+  console.log(req.body);
+
+  const mailOptions = {
+    from: `${name}`,
+    to: process.env.EMAIL_USER,
+    replyTo: email,
+    subject: `Email from ${name} `,
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+      res.status(500).json({ error: "Failed to send email" }); //
+    } else {
+      console.log("Email sent:", info.response);
+      res.status(200).json({ message: "Email sent successfully" }); //
+    }
+  });
+});
+app.listen("3000", console.log("listening to the port"));
